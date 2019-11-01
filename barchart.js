ea5238b9afd9150d542b087ce3ffca0c2591739e
RETURNNEVER <47939680+RETURNNEVER@users.noreply.github.com>
2019-11-01 10:50:05 +0800

    Update barchart.js

--- barchart.js
+++ barchart.js
@@ -1,22 +1,6 @@
-var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
-
 var svg = d3.select("body").append("svg")
           .attr("height","100%")
           .attr("width","100%");
-svg.selectAll("rect")
-    .data(dataArray)
-    .enter().append("rect")
-          .attr("class", "bar")
-          .attr("height", function(d, i) {return (d * 10)})
-          .attr("width","40")
-          .attr("x", function(d, i) {return (i * 60) + 25})
-          .attr("y", function(d, i) {return 400 - (d * 10)});
-svg.selectAll("text")
-    .data(dataArray)
-    .enter().append("text")
-    .text(function(d) {return d})
-          .attr("x", function(d, i) {return (i * 60) + 36})
-          .attr("y", function(d, i) {return 390 - (d * 10)});
 d3.csv("NSFC01.csv",function(error,data){
-    console.log(data);
+    console.log(data[0]);
 })
