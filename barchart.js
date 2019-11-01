990b0bca199190effcd2c462e7866e063eaefd89
RETURNNEVER <47939680+RETURNNEVER@users.noreply.github.com>
2019-11-01 21:37:57 +0800

    Update barchart.js

--- barchart.js
+++ barchart.js
@@ -1,7 +1,7 @@
 var svg = d3.select("body").append("svg")
           .attr("height","100%")
           .attr("width","100%")
-d3.csv("NSFC02.csv",function(data){
+d3.csv("NSFC01.csv",function(data){
     console.log(data);
 })
 var s={a:"ds",b:"sfa"};
